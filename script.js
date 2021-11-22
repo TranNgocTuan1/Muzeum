9441a95d60dbf7678b0c69eb0022f4b58b5cd084
TranNgocTuan1 <tomastuan12@gmail.com>
2021-11-22 20:20:48 +0100

    verze 1

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#owl-carousel').owlCarousel({
+        loop: true,
+        autoplay:true,
+        margin: 30,
+        dots: false,
+        //nav: true,
+        items: 1,
+        autoplayTimeout:7000
+    })
+
+
+    
+
+});
