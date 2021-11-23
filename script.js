5c2047c962b66ad588625cf90f57d3d8de973493
Tuan Tran Ngoc <tranngoc@SPSEJECNANET.CZ>
2021-11-23 10:30:46 +0100

    verze 2

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ $(document).ready(function(){
     })
 
 
+ 
     
 
 });
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
